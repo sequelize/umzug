67f99bcbee71da0f4a99d56edadc8b59e68bc6fe
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2017-05-10 23:00:32 +0300

    refactor(storages): fixed issue number in deprecation warnings

--- src/storages/json.js
+++ src/storages/json.js
@@ -5,5 +5,5 @@ module.exports = JSONStorage;
 console.warn(
   'Deprecated: JSONStorage\'s filename has changed!',
   'Use \'umzug/lib/storages/JSONStorage\' instead of \'umzug/lib/storages/json\'',
-  'For more information: https://github.com/sequelize/umzug/pull/138',
+  'For more information: https://github.com/sequelize/umzug/pull/139',
 );
