2e2c3cc174227d1f6c1f7ae970b54b1d13c7172a
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2017-05-10 23:00:32 +0300

    refactor(storages): fixed issue number in deprecation warnings

--- src/storages/none.js
+++ src/storages/none.js
@@ -5,5 +5,5 @@ module.exports = Storage;
 console.warn(
   'Deprecated: Storage\'s (former none storage) filename has changed!',
   'Use \'umzug/lib/storages/Storage\' instead of \'umzug/lib/storages/none\'',
-  'For more information: https://github.com/sequelize/umzug/pull/138',
+  'For more information: https://github.com/sequelize/umzug/pull/139',
 );
--- src/storages/sequelize.js
+++ src/storages/sequelize.js
@@ -5,5 +5,5 @@ module.exports = SequelizeStorage;
 console.warn(
   'Deprecated: SequelizeStorage\'s filename has changed!',
   'Use \'umzug/lib/storages/SequelizeStorage\' instead of \'umzug/lib/storages/sequelize\'',
-  'For more information: https://github.com/sequelize/umzug/pull/138',
+  'For more information: https://github.com/sequelize/umzug/pull/139',
 );
