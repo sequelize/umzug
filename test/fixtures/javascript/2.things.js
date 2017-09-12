7a61a3ac4758ed773e603a04dacb50b152f065a2
mmkal <mmkal@users.noreply.github.com>
2017-09-11 23:52:53 -0400

    fix: parentheses

--- test/fixtures/javascript/2.things.js
+++ test/fixtures/javascript/2.things.js
@@ -1,5 +1,5 @@
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 
-const sql = readFileSync(resolve(__dirname, '../sql/2.things.sql', 'utf8'));
+const sql = readFileSync(resolve(__dirname, '../sql/2.things.sql'), 'utf8');
 export const up = ({ sequelize }) => sequelize.query(sql);
