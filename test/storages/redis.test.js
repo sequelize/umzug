79a2d3004ada529ab5fd38e1ad889882b0baf493
Jens Stahle <jens.stahle@gmail.com>
2018-05-29 14:55:46 +0200

    removed redis dependecy from test

--- test/storages/redis.test.js
+++ test/storages/redis.test.js
@@ -2,7 +2,6 @@ import chai, {expect} from 'chai';
 import Storage from '../../src/storages/RedisStorage';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
-import redis from 'redis';
 
 chai.use(sinonChai);
 
@@ -11,29 +10,11 @@ describe('Redis', function () {
   let redisClient;
 
   before(function () {
-    if (process.env.REDIS_URL) {
-      redisClient = redis.createClient();
-      sinon.spy(redisClient, 'sadd');
-      sinon.spy(redisClient, 'srem');
-      sinon.spy(redisClient, 'smembers');
-    } else {
-      redisClient = {
-        sadd: sinon.stub().callsFake((key, member, callback) => callback(null)),
-        srem: sinon.stub().callsFake((key, member, callback) => callback(null)),
-        smembers: sinon.stub().callsFake((key, callback) => callback(null)),
-      };
-    }
-  });
-
-  after(function (done) {
-    if (process.env.REDIS_URL) {
-      redisClient.del(redisKey, (...args) => {
-        redisClient.end(true);
-        done();
-      });
-    } else {
-      done();
-    }
+    redisClient = {
+      sadd: sinon.stub().callsFake((key, member, callback) => callback(null)),
+      srem: sinon.stub().callsFake((key, member, callback) => callback(null)),
+      smembers: sinon.stub().callsFake((key, callback) => callback(null)),
+    };
   });
 
   it('should fail when connection is not set', function () {
@@ -79,37 +60,6 @@ describe('Redis', function () {
       return storage.executed()
         .then(migrations => {
           expect(redisClient.smembers.callCount).to.equal(1);
-        });
-    });
-  });
-  describe('test against redis database, pass via REDIS_URL', function () {
-    before(function () {
-      if (!process.env.REDIS_URL) {
-        this.skip();
-      }
-    });
-    it('should hold migration after logging', function () {
-      let storage = new Storage({client: redisClient, key: redisKey});
-      return storage.logMigration('MIGRATION')
-        .then(() => storage.executed())
-        .then(migrations => {
-          expect(migrations).to.eql(['MIGRATION']);
-        });
-    });
-    it('should hold migration only once after logging it twice', function () {
-      let storage = new Storage({client: redisClient, key: redisKey});
-      return storage.logMigration('MIGRATION')
-        .then(() => storage.executed())
-        .then(migrations => {
-          expect(migrations).to.eql(['MIGRATION']);
-        });
-    });
-    it('should not hold migration after unlogging', function () {
-      let storage = new Storage({client: redisClient, key: redisKey});
-      return storage.unlogMigration('MIGRATION')
-        .then(() => storage.executed())
-        .then(migrations => {
-          expect(migrations).to.eql([]);
         });
     });
   });
