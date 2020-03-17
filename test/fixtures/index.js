60dd2bb3d93412956a37a18a3bef030d2630c6a2
Sushant <sushantdhiman@outlook.com>
2020-03-17 12:25:30 +0530

    fix: use correct query type

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -44,7 +44,7 @@ describe('custom resolver', () => {
     this.verifyTables = async () => {
       const tables = await this.sequelize.query(
         'select * from sqlite_master where type=\'table\'',
-        { type: Sequelize.QueryTypes.RAW }
+        { type: Sequelize.QueryTypes.SHOWTABLES }
       );
 
       expect(tables.sort()).to.deep.equal(['SequelizeMeta', 'thing', 'user']);
