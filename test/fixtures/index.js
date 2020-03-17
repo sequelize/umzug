fd2606e8fa8f7929313d61936ecd11291151fd07
Sushant <sushantdhiman@outlook.com>
2020-03-17 12:16:47 +0530

    fix: use RAW options

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -42,7 +42,10 @@ describe('custom resolver', () => {
     };
 
     this.verifyTables = async () => {
-      const tables = await this.sequelize.query('select * from sqlite_master where type=\'table\'');
+      const tables = await this.sequelize.query(
+        'select * from sqlite_master where type=\'table\'',
+        { type: Sequelize.QueryTypes.RAW }
+      );
 
       expect(tables.sort()).to.deep.equal(['SequelizeMeta', 'thing', 'user']);
     };
