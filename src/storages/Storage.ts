d34fc03c33706cf47806c6c9123f81513bc9bc27
papb <papb1996@gmail.com>
2020-03-31 19:36:08 -0300

    Slowly refactor (01)

--- src/storages/Storage.ts
+++ src/storages/Storage.ts
@@ -1,35 +1,9 @@
-import Bluebird = require('bluebird');
-
-/**
- * @class Storage
- */
 export class Storage {
-  /**
-   * Does nothing.
-   *
-   * @param {String} migrationName - Name of migration to be logged.
-   * @returns {Promise}
-   */
-  logMigration (migrationName) {
-    return Bluebird.resolve();
-  }
+  async logMigration(migrationName: string): Promise<void> {}
 
-  /**
-   * Does nothing.
-   *
-   * @param {String} migrationName - Name of migration to unlog.
-   * @returns {Promise}
-   */
-  unlogMigration (migrationName) {
-    return Bluebird.resolve();
-  }
+  async unlogMigration(migrationName: string): Promise<void> {}
 
-  /**
-   * Does nothing.
-   *
-   * @returns {Promise.<String[]>}
-   */
-  executed () {
-    return Bluebird.resolve([]);
+  async executed(): Promise<string[]> {
+    return [];
   }
 }
