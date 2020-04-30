3640b1bbd0679a6cfbf9636d7afe30231f7e5407
mmkal <misha.kaletsky@gmail.com>
2020-04-30 13:35:22 -0400

    fix: codemod sorting test

--- test/test.ts
+++ test/test.ts
@@ -76,7 +76,7 @@ test('migrationsList() works', async () => {
 	await jetpack.removeAsync('umzug.json');
 });
 
-test.serial('migration sort', async t => {
+test('migration sort', async () => {
 	const executed = [];
 
 	await jetpack.removeAsync('umzug.json');
@@ -117,31 +117,31 @@ test.serial('migration sort', async t => {
 	const names = (migrations: Migration[]) => migrations.map(m => m.file);
 
 	// Before anything
-	t.deepEqual(executed, []);
-	t.deepEqual(names(await umzug.executed()), []);
-	t.deepEqual(names(await umzug.pending()), ['01-second-migration', '00-first-migration']);
+	expect(executed).toEqual([]);
+	expect(names(await umzug.executed())).toEqual([]);
+	expect(names(await umzug.pending())).toEqual(['01-second-migration', '00-first-migration']);
 
 	// Up: all at once
-	t.deepEqual(names(await umzug.up()), ['01-second-migration', '00-first-migration']);
-	t.deepEqual(executed, ['01-second-migration-up', '00-first-migration-up']);
-	t.deepEqual(names(await umzug.executed()), ['01-second-migration', '00-first-migration']);
-	t.deepEqual(names(await umzug.pending()), []);
+	expect(names(await umzug.up())).toEqual(['01-second-migration', '00-first-migration']);
+	expect(executed).toEqual(['01-second-migration-up', '00-first-migration-up']);
+	expect(names(await umzug.executed())).toEqual(['01-second-migration', '00-first-migration']);
+	expect(names(await umzug.pending())).toEqual([]);
 
 	// Down: one by one
-	t.deepEqual(names(await umzug.down()), ['00-first-migration']);
-	t.deepEqual(executed, ['01-second-migration-up', '00-first-migration-up', '00-first-migration-down']);
-	t.deepEqual(names(await umzug.executed()), ['01-second-migration']);
-	t.deepEqual(names(await umzug.pending()), ['00-first-migration']);
+	expect(names(await umzug.down())).toEqual(['00-first-migration']);
+	expect(executed).toEqual(['01-second-migration-up', '00-first-migration-up', '00-first-migration-down']);
+	expect(names(await umzug.executed())).toEqual(['01-second-migration']);
+	expect(names(await umzug.pending())).toEqual(['00-first-migration']);
 
-	t.deepEqual(names(await umzug.down()), ['01-second-migration']);
-	t.deepEqual(executed, [
+	expect(names(await umzug.down())).toEqual(['01-second-migration']);
+	expect(executed).toEqual([
 		'01-second-migration-up',
 		'00-first-migration-up',
 		'00-first-migration-down',
 		'01-second-migration-down',
 	]);
-	t.deepEqual(names(await umzug.executed()), []);
-	t.deepEqual(names(await umzug.pending()), ['01-second-migration', '00-first-migration']);
+	expect(names(await umzug.executed())).toEqual([]);
+	expect(names(await umzug.pending())).toEqual(['01-second-migration', '00-first-migration']);
 
 	await jetpack.removeAsync('umzug.json');
 });
