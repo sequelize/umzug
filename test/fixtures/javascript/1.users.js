f26ef47653981577ef7d80720341a9b481cf6d31
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2020-05-23 15:00:05 -0400

    test: events (#225)
    * test: events
    
    also decouple js from sql test fixtures
    
    * use `.toMatchObject` instead of inline snapshot
    
    * don't use triple-nested arrays
    
    * fix: typo

--- test/fixtures/javascript/1.users.js
+++ test/fixtures/javascript/1.users.js
@@ -1,7 +1,6 @@
-const { readFileSync } = require('fs');
-const { resolve } = require('path');
-
-const sql = readFileSync(resolve(__dirname, '../sql/1.users.sql'), 'utf8');
-const up = ({ sequelize }) => sequelize.query(sql);
-
-module.exports = { up };
+exports.up = ({ sequelize }) => sequelize.query(`
+  CREATE TABLE user (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR
+  );
+`);
