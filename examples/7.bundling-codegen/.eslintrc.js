f77fc8b671255ad7a669c5ab6ac9b13d14ed7be3
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2020-12-09 15:47:30 -0500

    Add bundling example (#415)

---
+++ examples/7.bundling-codegen/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+		project: ['./tsconfig.json'],
+	},
+  plugins: ['codegen'],
+  ignorePatterns: ['.eslintrc.js', 'migrate.js'],
+  rules: {
+    'codegen/codegen': 'error',
+  }
+}
