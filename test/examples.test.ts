67b55ccc44f8f9290138a3d9ae5955a66c98f79d
Misha Kaletsky <mmkal@users.noreply.github.com>
2023-08-03 15:56:43 +0200

    scrub cwd

--- test/examples.test.ts
+++ test/examples.test.ts
@@ -6,6 +6,14 @@ import stripAnsi from 'strip-ansi';
 const examplesDir = path.join(__dirname, '../examples');
 const examples = fs.readdirSync(examplesDir).filter(ex => /^\d/.exec(ex));
 
+/** get rid of any untracked files, including newly-created migrations and the sqlite db file, so that each run is from scratch and has the same output (give or take timings etc.) */
+const cleanup = (cwd: string) => {
+	childProcess.execSync('git checkout migrations && git clean migrations seeders db.sqlite -fx', {
+		cwd,
+		stdio: 'inherit',
+	});
+};
+
 examples.forEach(ex => {
 	test(`example ${ex}`, async () => {
 		const dir = path.join(examplesDir, ex);
@@ -13,15 +21,7 @@ examples.forEach(ex => {
 		const readme = fs.readFileSync(path.join(dir, readmeFile!)).toString();
 		const bash = readme.split('```bash')[1].split('```')[0].trim();
 
-		/** get rid of any untracked files, including newly-created migrations and the sqlite db file, so that each run is from scratch and has the same output (give or take timings etc.) */
-		const cleanup = () => {
-			childProcess.execSync('git checkout migrations && git clean migrations seeders db.sqlite -fx', {
-				cwd: dir,
-				stdio: 'inherit',
-			});
-		};
-
-		cleanup();
+		cleanup(dir);
 
 		const stdout = bash
 			.split('\n')
@@ -33,13 +33,13 @@ examples.forEach(ex => {
 			})
 			.map(s => stripAnsi(s))
 			.join('\n\n')
+			.split(process.cwd())
+			.join('<<cwd>>')
 			.replace(/\d{4}.\d{2}.\d{2}T\d{2}.\d{2}.\d{2}/g, '<<timestamp>>')
 			.replace(/durationSeconds: .*/g, 'durationSeconds: ???')
 			.replace(/\d+kB (.*)/g, '???kB $1')
 			.replace(/\[\d+ms] - ncc/g, '[????ms] - ncc')
 			.replace(/up to date, audited \d+ packages in \w+s/g, 'up to date, audited ??? packages in ???ms');
-
-		cleanup();
 
 		expect(stdout).toMatchSnapshot();
 	});
