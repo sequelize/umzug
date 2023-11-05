4cbf4da5e885fabbd2a06cedfddf9ea18697282b
Misha Kaletsky <mmkal@users.noreply.github.com>
2023-11-04 22:33:32 -0400

    slash

--- examples/0.vanilla/migrate.js
+++ examples/0.vanilla/migrate.js
@@ -5,7 +5,7 @@ exports.migrator = new Umzug({
 		glob: 'migrations/*.js',
 	},
 	context: { directory: __dirname + '/ignoreme' },
-	storage: new JSONStorage({ path: 'ignoreme/storage.json' }),
+	storage: new JSONStorage({ path: __dirname + '/ignoreme/storage.json' }),
 	logger: console,
 });
 
