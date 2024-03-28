e4f786506ed670b9485812902522aa1e4163eb55
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2024-03-28 14:47:09 -0400

    test lower node versions properly (#661)
    previous CI setup wasn't actually running, it was throwing an uncaught
    rejection and somehow exiting with code 0 anyway
    
    getting vitest to work on node 12 was miles away from working, so moving
    the node version tests to the `test_tgz` job, which is a simpler test
    that ensures the basics work.
    
    Also adds some small fixes to the library itself to get it _closer_ to
    working, but the `glob` dependency is the sticking point for node 12.
    14, 16, 18 are now fine.
    
    `glob` dependency will be replaced in #660 
    
    If getting node 12 to work proves impossible, we might need to just go
    to a v4 release but hopefully not.

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,11 @@
 module.exports = [
   ...require('eslint-plugin-mmkal').recommendedFlatConfigs,
   {ignores: ['lib/**', 'examples/**', 'test/generated/**']}, //
+  {
+    rules: {
+      // todo[>=4.0.0] drop lower node version support and remove these
+      'unicorn/prefer-string-replace-all': 'off',
+      'unicorn/prefer-at': 'off',
+    },
+  },
 ]
